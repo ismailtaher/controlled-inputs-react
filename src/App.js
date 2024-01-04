a769f3c13d827b9964e2b7531ba52c4f2f7db970
ismailtaher <ismail.taher@ymail.com>
2024-01-04 20:53:14 +0500

    4th Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
@@ -22,6 +23,8 @@ function App() {
     },
   ]);
 
+  const [newItem, setNewItem] = useState("");
+
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
@@ -36,9 +39,17 @@ function App() {
     localStorage.setItem("shoppingList", JSON.stringify(listItems));
   };
 
+  const handleSubmit = (e) => {
+    console.log("submitted");
+  };
+
   return (
     <div className="App">
       <Header title="Groceries List"></Header>
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}></AddItem>
       <Content
         items={items}
         handleCheck={handleCheck}
